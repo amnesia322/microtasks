7e12afdc2959ae25ccb6e67d39dae2101a04cdfd
amnesia322 <dev.amnesia322@gmail.com>
2022-08-23 18:47:45 +0300

    learn button prt.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./site/Header";
-import {Body} from "./site/Body"
-import {Footer} from "./site/Footer";
-import {NewComponent} from "./site/NewComponent";
-import {CarsTable} from "./site/CarsTable";
-
-//BLL
-const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-]
-
-const topCars = [
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'}
-]
 
 
 // UI
 function App() {
-    return (
-        <>
-            <CarsTable list={topCars}/>
-            <hr/>
-            <NewComponent students={students}/>
-            <hr/>
+    /*const myFirstSubscriber = () => {
+        console.log("Hello! I`m Vasya!")
+    }
+    const mySecondSubscriber = () => {
+        console.log("Hello! I`m Ivan!")
+    }*/
 
-            <Header title='New Body'/>
-            <Body titleForBody='New Body'/>
-            <Footer titleForFooter='New Body'/>
-        </>
+      const onClickHandler = (name: string) => {
+          return console.log(name)
+      }
+
+
+    return (
+        <div>
+            <button onClick={()=>onClickHandler('Vasya')}>MyYouTubeChanel - 1</button>
+            <button onClick={()=>onClickHandler('Ivan')}>MyYouTubeChanel - 2</button>
+
+
+        </div>
     );
 }
 
