559638b168df9eece29dd99132b277236b03ae8b
amnesia322 <dev.amnesia322@gmail.com>
2022-08-24 13:06:49 +0300

    add FullInput.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
 import React, {useState} from 'react';
 import './App.css';
-import {NewComponent} from "./NewComponent";
+import {FullInput} from "./components/FullInput";
 
-export type FilterType = 'all' | 'dollar' | 'ruble';
+
 
 function App() {
-    const [money, setMoney] = useState([
-        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
-        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
-        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
     ])
 
-    let [filter, setFilter] = useState<FilterType>("all")
-
-
-    let currentMoney = money;
-    if (filter === "ruble") {
-        currentMoney = money.filter(m=> m.banknote === "ruble")
-    }
-    if (filter === "dollar") {
-        currentMoney = money.filter(m=> m.banknote === 'dollar')
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
     }
 
 
-    const onClickHandler = (nameButton: FilterType) => {
-        setFilter(nameButton)
-    }
+   
     return (
-        <NewComponent money={currentMoney} onClickHandler={onClickHandler}/>
+        <div>
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
+                return <div key={index}>{el.message}</div>
+            })}
+        </div>
     );
 }
 
